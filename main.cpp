f7ac22dd5b8db56337a851e902b82ff98d08d32e
Sijia Gao <sijia.gao.biogeo@gmail.com>
2024-05-04 21:18:47 +0200

    added functions in c++

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@ consteval int get_value() {
     return 3;
 }
 
+int addNumbers(int first_number, int second_number){
+    int sum = first_number + second_number;
+    return sum;
+}
 
 int main(int argc, char **argv){
     // constexpr int value = get_value();
 
     // Statement in c++
-    int firstNumber = 12;
-    int secondNumber = 9;
-    int sum = firstNumber + secondNumber;
-    std::cout << "The sum of the two numbers is : " << sum << std::endl;
+    int summation = addNumbers(-2, 1);
+    std::cout << "The sum of the two numbers is : " << summation << std::endl;
 }
 
 /*
