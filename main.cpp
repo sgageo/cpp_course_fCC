d1a98ed728124976c7f0d7ceeca20ad05d238ba1
Sijia Gao <sijia.gao.biogeo@gmail.com>
2024-05-11 16:08:26 +0200

    tested text-input function that contains spaces in texts

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 // To load the iostream library
 #include <iostream>
+#include <string>
 
 consteval int get_value() {
     return 3;
 }
 
-int addNumbers(int first_number, int second_number){
-    int sum = first_number + second_number;
+int addNumbers(int first_param, int second_param){
+    int sum = first_param + second_param;
     return sum;
+}
+
+std::string getNames(){
+    std::string full_name;
+    std::cout << "Please type in your full name:" 
+        << std::endl;
+    // Getting data with spaces
+    std::getline(std::cin, full_name);
+    return full_name;
 }
 
 int main(int argc, char **argv){
@@ -15,7 +25,10 @@ int main(int argc, char **argv){
 
     // Statement in c++
     int summation = addNumbers(-2, 1);
-    std::cout << "The sum of the two numbers is : " << summation << std::endl;
+    std::cout << "The sum of the two numbers is : " 
+        << summation << std::endl;
+    std::string Name = getNames();
+    std::cout << "Hello, " << Name << std::endl;
 }
 
 /*
